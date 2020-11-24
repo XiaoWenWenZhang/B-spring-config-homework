f6f874e766bdb9743da40cdc2a2b4ee8068305c2
xiaowen <846703636@qq.com>
2020-11-24 09:56:00 +0800

    finish p2s2

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/AdvancedLevelController.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestPropertySource
+public class AdvancedLevelController {
+    @Autowired
+    LevelController levelController;
+
+    @Test
+    void should_return_advanced_when_level_number_less_than_1(){
+        assertEquals("advanced",levelController.getLevel());
+    }
+}
